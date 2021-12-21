f53c9e293994292b163fc1961473c52ab29bf9b7
Arkingco <Arkingco@gmail.com>
2021-12-21 20:57:19 +0900

    gnl

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:14:40 by kipark            #+#    #+#             */
-/*   Updated: 2021/12/15 21:32:14 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2021/12/21 15:57:41 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"get_next_line.h"
 #include<stdio.h>
 
-#define BUFFER_SIZE 2
+#define BUFFER_SIZE 100
 
 g_list *make_buffer_list(g_list *buffer_list)
 {
@@ -104,7 +104,7 @@ void print_list(g_list *bf_list)
 	printf("\n");
 }
 
-char *get_next_line(int fd)
+char *get_next_linelist(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
 	g_list		*bf_list;
@@ -130,4 +130,58 @@ char *get_next_line(int fd)
 		return (0);
 	printf("%s\n", get_line);
 	return (0);
+}
+
+///
+///		list로 만든 get_next_line
+///
+
+
+///
+///		malloc 으로 만든 get_next_line
+///
+
+char *set_get_line_char(char *get_line, char *buffer, int buffer_length)
+{
+	char	*new_line;
+	int	get_line_length;
+	int i;
+
+	i = 0;
+	get_line_length = ft_strlen(get_line);
+	new_line = malloc(sizeof(char) * (get_line_length + buffer_length + 1));
+	if(new_line == NULL)
+		return (0);
+	while(i < get_line_length)
+	{
+		new_line[i] = get_line[i];
+		++i;
+	}
+	while(i < buffer_length + get_line_length)
+	{
+		new_line[i] = buffer[i - get_line_length];
+		++i;
+	}
+	new_line[i] = '\0';
+	return (new_line);
+}
+
+char *get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	char		*get_line;
+
+	get_line = "";
+	while ((read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		if (ft_strchr(buffer, '\n') != 0)
+		{
+			get_line = set_get_line_char(get_line, buffer, ft_strchr(buffer, '\n'));
+			break;
+		}
+		get_line = set_get_line_char(get_line, buffer, BUFFER_SIZE);
+	}
+	printf("%s", get_line);
+
+	return (get_line);
 }
--- gnl/main.c
+++ gnl/main.c
@@ -6,7 +6,7 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 14:21:40 by kipark            #+#    #+#             */
-/*   Updated: 2021/12/15 20:44:27 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2021/12/21 20:56:36 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 #include<stdio.h>
 #include<fcntl.h>
 
+#define BUFFER_SIZE 40
+
 int main(int argc, char *argv[])
 {	
 	int i;
 	int fd;
 	int read_byte;
-	
+	char buffer[BUFFER_SIZE];
+
+
 	i = 1;
 	read_byte = 0;
-	while(i < argc)
+	while(i < argc)  	
 	{
 		if(0 < (fd = open(argv[1], O_RDONLY)))
 		{
-			printf("count\n");
 			get_next_line(fd);
 		}
 		++i;
+		
 	}
 }
