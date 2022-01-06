072396cbfa17183dd811c2786b3ca6fea89b9dbd
Arkingco <Arkingco@gmail.com>
2022-01-06 20:57:26 +0900

    gnl

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 15:14:40 by kipark            #+#    #+#             */
-/*   Updated: 2022/01/06 20:17:36 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2022/01/06 20:55:17 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,55 @@ char *gnl_set_line(char *static_line, char *buffer, int fd)
 	{
 		if (gnl_strchr(buffer, '\n') != 0)
 		{
-			static_line = gnl_strjoin(static_line, buffer, gnl_strchr(buffer, '\n'));
+			static_line = gnl_strjoin(static_line, buffer, BUFFER_SIZE);
 			break;
 		}
 		static_line = gnl_strjoin(static_line, buffer, BUFFER_SIZE);
 	}
-	printf("\n%s\n", static_line);
 
 	return (static_line);
 }
 
 char *gnl_set_return_line(char *static_line, char *return_line)
 {
-	return_line = 
+	int return_line_length;
+	int i;
+
+	return_line_length = gnl_strchr(static_line, '\n');
+	return_line = malloc(sizeof(char) * return_line_length + 1);
+	if(return_line == 0)
+		return (0);
+	i = 0;
+	while(i < return_line_length)
+	{
+		return_line[i] = static_line[i];
+		++i;
+	}
+	return_line[i] = '\0';
+	return (return_line);
+}
+
+char *gnl_set_static_line(char *static_line)
+{
+	char *new_static_line;
+	int new_static_line_length;
+	int static_line_length;
+	int i;
+
+	i = 0;
+	static_line_length = gnl_strlen(static_line);
+	new_static_line_length = gnl_strlen(static_line) - gnl_strchr(static_line, '\n');
+	new_static_line = malloc(sizeof(char) * static_line_length + 1);
+	if(new_static_line == 0)
+		return (0);
+	while(i < new_static_line_length)
+	{
+		new_static_line[i] = static_line[i + static_line_length];
+		++i;
+	}
+	new_static_line[i] = '\0';
+	free(static_line);
+	return (new_static_line);
 }
 
 char *get_next_line(int fd)
@@ -48,12 +84,14 @@ char *get_next_line(int fd)
 	 
 	if(fd < 0 || BUFFER_SIZE < 1)
 		return (0);
-	if(static_line == NULL || static_line = "")
+	if(static_line == NULL || gnl_strchr(static_line, '\n') == 0)
 	{
 		static_line = gnl_set_line(static_line, buffer, fd);
+		printf("\n\n%s\n\n", static_line);
 	}
+	return_line = NULL;
+	return_line = gnl_set_return_line(static_line, return_line);
+	static_line = gnl_set_static_line(static_line);
 	
-	
-
-	return (static_line);
+	return (return_line);
 }
