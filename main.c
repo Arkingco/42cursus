9695497aebc13aafb58d7ce3b3c3bd891926f7dc
Arkingco <Arkingco@gmail.com>
2022-01-20 21:51:03 +0900

    printf study

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 #include<stdarg.h>
 
+// 가변 인자를 사용하는데 쓰이는 매크로가 몇 가지 존재합니다.
+
+// va_start
+// va_arg
+// va_list
+// va_end
+
+int test(int nSize, ...)
+{
+	return (0);
+}
+
 int main()
 {
-
+	printf("hi");
 }
