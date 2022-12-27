8520d50cceed2939ae09467162c2aac625528f8c
arkingco <arkingco@gmail.com>
2022-12-27 17:54:16 +0900

    feat : ex04 구현

---
+++ 42_cpp_module/CPP01/ex04/main.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+
+// 1. 입력에 대한 에러 핸들링이 필요
+// 2. s1의 내용을 s2로 고치기
+int main(int argc, char** argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "please input 3 parameter! (filename, s1, s2)" << std::endl;
+        return 0;
+    }
+
+    std::string file_name = argv[1];
+    std::string file_content;
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream file(file_name);
+    if (file.is_open())
+    {
+        file.seekg(0, std::ios::end);
+        int size = file.tellg();
+        file_content.resize(size);
+        file.seekg(0, std::ios::beg);
+        file.read(&file_content[0], size);
+
+        std::cout << "------------- Before ----------------------" << std::endl;
+        std::cout << file_content << std::endl;
+
+        int loc_s1 = 0;
+        int s1_size = s1.size();
+        while ((loc_s1 = file_content.find(s1)) != -1)
+        {
+            file_content.erase(loc_s1, s1_size);
+            file_content.insert(loc_s1, s2);
+        }
+        std::cout << "------------- After -----------------------" << std::endl;
+        std::cout << file_content << std::endl;
+    }
+    else
+    {
+        std::cout << "cannot find file! " << std::endl;
+    }
+}
