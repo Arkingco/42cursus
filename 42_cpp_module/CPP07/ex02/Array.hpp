4af4162e0c4555b89e25ff8515249ff78a7a2e71
arkingco <arkingco@gmail.com>
2023-01-21 18:40:04 +0900

    CPP07 gogo

--- 42_cpp_module/CPP07/ex02/Array.hpp
+++ 42_cpp_module/CPP07/ex02/Array.hpp
@@ -3,18 +3,78 @@
 
 #include <iostream>
 
+template<typename T>
 class Array
 {
   private:
-    int *value;
+    T 				*value;
+    unsigned int 	length;
 
   public:
-    Array();
-    Array(int n);
-    Array(const Array& a);
-    ~Array();
-    Array& operator=(const Array& a);
+    Array() : value(NULL), length(0)
+    {
+   	   std::cout << "Default constructor Call" << std::endl;
+    }
 
+    Array(unsigned int n) : value(new T[n]), length(n)
+    {
+   	   std::cout << "constructor Call" << std::endl;
+    }
+
+    Array(const Array& other) : value(new T[other.length]), length(other.length)
+    {
+		for (unsigned int i=0; i<other.length; ++i)
+			value[i] = other.value[i];
+		length = other.length;
+
+		std::cout << "Copy constructor Call" << std::endl;
+    }
+
+    Array& operator=(const Array& other)
+    {
+		if (this == &other)
+			return *this;
+		std::cout << "1" << std::endl;
+		if (value != NULL)
+			delete[] value;
+		std::cout << "1" << std::endl;
+		value = new T[other.length];
+		for (unsigned int i=0; i<other.length; ++i)
+			value[i] = other.value[i];
+		length = other.length;
+		std::cout << "1" << std::endl;
+
+		std::cout << "Array Assignment Operator Call" << std::endl;
+		return *this;
+    }
+
+    T& operator[] (unsigned int idx) const
+    {
+		if(idx < 0 || idx >= length)
+			throw OutOfBindException();
+		return value[idx];
+    }
+
+	unsigned int size() const
+	{
+		return length;
+	}
+
+	class OutOfBindException : public std::exception
+	{
+		virtual const char * what() const throw(){
+			return "out of bound!";
+		}
+	};
 };
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const Array<T>& obj)
+{
+	for (unsigned int i = 0; i<obj.size(); ++i)
+		os << obj[i] << " ";
+	os << std::endl;
+	return(os);
+}
 
 #endif
--- 42_cpp_module/CPP07/ex02/main.cpp
+++ 42_cpp_module/CPP07/ex02/main.cpp
@@ -1,24 +1,79 @@
-#include "Iter.hpp"
-
-template<typename T>
-void print_array(T t)
-{
-	std::cout << t << std::endl;
-}
+#include "Array.hpp"
+#include <stdlib.h>
 
 int main( void ) {
 	
-	int a[5] = {1,2,3,4,5};
-	iter(a, 5, &print_array);
-	std::cout << std::endl;
+	int n = 5;
+	try
+	{
+		Array<int> a(n);
+
+		for (int i=0; i<n; ++i)
+			a[i] = i + 1;
+		std::cout << a << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		Array<std::string> b(n);
+
+		std::cout << b[n + 1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		Array<std::string> b(n);
+
+		for (int i=0; i<n; ++i)
+			b[i] = "string!";
+		std::cout << b << std::endl; 
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		Array<int> c(n);
+		for (int i=0; i<n; ++i)
+			c[i] = i + 1;
+
+		Array<int> d(c);
+		for (int i=0; i<n; ++i)
+			d[i] = d[i] + 5;
+		
+		std::cout << c << d << std::endl;
+	} 
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		Array<int> f;
+		Array<int> e(n);
+		for (int i=0; i<n; ++i)
+			e[i] = i + 1;
+
+		f = e;
 	
-	std::string b[5] = {"a", "b", "c", "d", "e"};
-	iter(b, 5, &print_array);
-	std::cout << std::endl;
-
-	double c[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
-	iter(c, 5, &print_array);
-	std::cout << std::endl;
-
+		for (int i=0; i<n; ++i)
+			f[i] = f[i] + 10;
+		std::cout << f;
+	} 
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return 0;
 }
