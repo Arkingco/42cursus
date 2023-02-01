7b5eb5b46d15b3d4d61152b55d5880961d0c6220
kipark <arkingco@gmail.com>
2023-02-01 18:09:05 +0900

    operator [], at implement

--- 42_ft_containers/main.cpp
+++ 42_ft_containers/main.cpp
@@ -9,21 +9,27 @@ int main()
 {
     ft::vector<int> ft_vector;
     std::vector<int> std_vector;
-    ft_vector.push_back(10);
-    ft_vector.push_back(10);
-    ft_vector.push_back(10);
-    ft_vector.push_back(10);
-    std_vector.push_back(10);
-    std_vector.push_back(10);
-    std_vector.push_back(10);
-    std_vector.push_back(10);
-    std_vector.push_back(10);
+    ft_vector.push_back(1);
+    ft_vector.push_back(2);
+    ft_vector.push_back(3);
+    ft_vector.push_back(4);
+    std_vector.push_back(1);
+    std_vector.push_back(2);
+    std_vector.push_back(3);
+    std_vector.push_back(4);
 
     for(ft::vector<int>::iterator i = ft_vector.begin(); i != ft_vector.end(); ++i)
     {
         std::cout << *i << std::endl;
     }
 
-    ft::is_integral<volatile int> a;
-    std::cout << a.value << std::endl;
+    ft_vector[0] = 10;
+    std::cout << ft_vector[0] << " " << ft_vector[1] << " " << ft_vector[2] << " " << ft_vector[3] << std::endl;
+
+    ft_vector.at(10) = 10;
+    std::cout << ft_vector[0] << " " << ft_vector[1] << " " << ft_vector[2] << " " << ft_vector[3] << std::endl;
+
+    // std::cout << ft_vector[-1] << " " << ft_vector[-2] << " " << ft_vector[5] << " " << ft_vector[4] << std::endl;
+    // have to clear this problem
+    // ft::is_integral<const int> a;   ft::is_integral<volatile int> a;
 }
--- 42_ft_containers/vector.hpp
+++ 42_ft_containers/vector.hpp
@@ -136,10 +136,30 @@ class vector
         size_type capacity() const {return size_type(_end_cap - _begin);}
 
         // // Element access:
-        // reference       operator[](size_type n);
-        // const_reference operator[](size_type n) const;
-        // reference       at(size_type n);
-        // const_reference at(size_type n) const;
+        reference       operator[](size_type n)
+        {
+          if (n >= _end_cap - _begin || n < 0)
+            return *(_begin);
+          return *(_begin + n);
+        }
+        const_reference operator[](size_type n) const
+        {
+          if (n >= _end_cap - _begin || n < 0)
+            return *(_begin);
+          return *(_begin + n);
+        }
+        reference       at(size_type n)
+        {
+          if (n >= _end_cap - _begin || n < 0)
+            return *(_begin);
+          return *(_begin + n);
+        }
+        const_reference at(size_type n) const
+        {
+          if (n >= _end_cap - _begin || n < 0)
+            return *(_begin);
+          return *(_begin + n);
+        }
 
         // reference       front();
         // const_reference front() const;
