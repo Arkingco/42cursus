bb773b186bc4cc8db628537f796d1850e3970022
arkingco <arkingco@gmail.com>
2023-03-19 06:58:18 +0900

    connect wordpress mariadb

--- 42_inception/srcs/requirements/mariadb/script/mariadb.sh
+++ 42_inception/srcs/requirements/mariadb/script/mariadb.sh
@@ -1,7 +1,19 @@
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-# /usr/bin/mysqld --user=mysql << EOF
-# USE mysql;
-# EOF
+/usr/bin/mysqld --user=mysql --bootstrap << EOF
+use mysql;
+flush privileges;
 
-exec /usr/bin/mysqld --user=mysql --console
+alter user 'root'@'localhost' identified by '$DB_ROOT_PASSWORD';
+flush privileges;
+
+create database $DB_NAME;
+create user '$DB_USER'@'%' identified by '$DB_USER_PASSWORD';
+grant all privileges on $DB_NAME.* to '$DB_USER'@'%';
+
+flush privileges;
+EOF
+
+echo this is endpoint!
+
+exec /usr/bin/mysqld --user=mysql --console	
---
+++ 42_inception/srcs/requirements/wordpress/script/wordpress.sh
@@ -0,0 +1,3 @@
+mv /tmp/wp-config.php /var/www/localhost/htdocs/wp-config.php
+
+/usr/sbin/php-fpm81 -F -R
