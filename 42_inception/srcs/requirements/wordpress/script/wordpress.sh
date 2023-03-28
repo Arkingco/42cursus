5abe672e4e0633964f388e5d80454b08792ad8f7
arkingco <arkingco@gmail.com>
2023-03-28 21:23:58 +0900

    inception

--- 42_inception/srcs/requirements/wordpress/script/wordpress.sh
+++ 42_inception/srcs/requirements/wordpress/script/wordpress.sh
@@ -1,3 +1,14 @@
-mv /tmp/wp-config.php /var/www/localhost/htdocs/wp-config.php
+wp core download --allow-root --path=/var/www/html
 
-/usr/sbin/php-fpm81 -F -R
+               
+wp	core install \
+	--allow-root \
+	--path=/var/www/html \
+	--url="kipark.42.fr" \
+	--title=Inception \
+    --admin_user=your_admin_username="kipark" \
+    --admin_password="1234" \
+    --admin_email=your_admin_email="arkingco@gmail.com" \
+	--skip-email
+
+/usr/sbin/php-fpm8 -F -R
